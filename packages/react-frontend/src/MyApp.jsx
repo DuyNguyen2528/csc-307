f8c1f14b64bb9f7f13a40d645beb81b8e8d99329
Duy Nguyen <dylannguyen@Duys-MacBook-Pro.local>
2024-10-01 15:57:31 -0700

    summit form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,14 +6,20 @@ import Form from "./Form";
 
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState([    {
+        name: "Charlie",
+        job: "Janitor" // the rest of the data
+      }
+  ]);
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
         return i !== index;
         });
         setCharacters(updated);
     }
-
+    function updateList(person){
+        setCharacters([...characters, person]);
+    }
   return (
     
     <div className="container">
@@ -21,7 +27,8 @@ function MyApp() {
             characterData={characters} 
             removeCharacter={removeOneCharacter}
         />
-        <Form/> 
+        <Form handleSubmit = {updateList}/> 
+        
     </div>
   );
 }
