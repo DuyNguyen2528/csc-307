6974ce074c51b6c60f337c23cadc674e9ab10956
Duy Nguyen <dylannguyen@Duys-MacBook-Pro.local>
2024-10-10 18:14:28 -0700

    backend and frontend sucessfuly communicate

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,27 +1,46 @@
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Table from "./Table";
 import Form from "./Form";
 
 
 
 function MyApp() {
-    const [characters, setCharacters] = useState([    {
-        name: "Charlie",
-        job: "Janitor" // the rest of the data
-      }
-  ]);
+
+    ///////// STAGE LIST //////////////
+    const [characters, setCharacters] = useState([]);
+
+
+    ///////// HELPER FUNCTION /////////////
+    //--------------fetchUser()------------------
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    //--------------useEffect()------------------
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+      }, [] );
+
+    //--------------removeOneCharacter(index)------------------
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
         return i !== index;
         });
         setCharacters(updated);
     }
+
+    //--------------updateList(person)------------------
     function updateList(person){
         setCharacters([...characters, person]);
     }
+
+    //--------------return------------------
   return (
-    
     <div className="container">
         <Table 
             characterData={characters} 
